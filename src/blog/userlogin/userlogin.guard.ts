97e4a20c60f1a32878ec50435661e97f8a9d3ce9
amir ahamdzadeh <amirahmadzadeh944@gmail.com>
2023-08-11 11:54:15 +0330

    created gurad userLogin

---
+++ src/blog/userlogin/userlogin.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable  } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
+
+
+@Injectable()
+export class UserloginGuard implements CanActivate {
+
+  constructor(private readonly authService : AuthService){}
+
+  canActivate(context: ExecutionContext,): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+
+    const auth = request.headers?.authorization
+    
+    return this.authService.validateToken(auth);
+  }
+}
