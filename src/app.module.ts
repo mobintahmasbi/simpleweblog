4d6b08d3611f2f0beb46f79827b2ce3e03b18c4f
mobin <mobintahmasbi13801202@gmail.com>
2023-08-11 10:48:15 +0430

    add connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CategoryModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '1380',
+      database: 'simpleweblog',
+      autoLoadEntities: true,
+      synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
